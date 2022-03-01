4d0f021ccd0ac9d91b9bb3ad0d916c1ba172209b
OndrejKocak <ondrej.kocak@gmail.com>
2022-03-01 17:09:04 +0100

    check sum bez pouzitia array

--- main.c
+++ main.c
@@ -3,21 +3,20 @@
 
 int uloha1_6(){
     char checksum_nacitany[2];
-    int checksum_vypocitany=0, dlzka_dat, dlzka_riadku;
+    int checksum_vypocitany=0, dlzka_dat, dlzka_riadku, x=0;
     scanf(":%2x", &dlzka_dat);
     dlzka_riadku = dlzka_dat +4;
+    checksum_vypocitany = dlzka_dat;
 
-    int hex_line[dlzka_riadku];
-    hex_line[0] = dlzka_dat;
-    for(int i=1;i<=dlzka_riadku;i++){
-        scanf("%2x", &hex_line[i]);
+    for(int i=1;i<dlzka_riadku;i++){
+        scanf("%2x", &x);
+        checksum_vypocitany =checksum_vypocitany + x;
     }
     scanf("%2x", &checksum_nacitany);
-    printf("---nacitane---");
-    for(int i=0;i<=dlzka_riadku;i++){
-        checksum_vypocitany = checksum_vypocitany + hex_line[i];
-        printf("%x\n", checksum_vypocitany);
-    }
+    printf("---nacitane---\n");
+
+    printf("%x\n", checksum_vypocitany);
+    printf("%X", 255-(checksum_vypocitany%256)+1);
 }
 
 
